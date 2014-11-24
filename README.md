5df45be96fa904d36ce32704c4d64697edfbe45f
DalInar <oryx@umich.edu>
2014-11-24 14:34:10 -0500

    Set up basic MPI

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ ParPaths
 ========
 
 Variety of parallel algorithms for finding shortest paths in a (possibly changing) graph
+
+
+BUILD
+========
+cmake -G "Eclipse CDT4 - Unix Makefiles" -D CMAKE_BUILD_TYPE=Debug .
+make
+
+Can change build type to Release or Debug in CMakeLists.txt
--- parpaths.cpp
+++ parpaths.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include <mpi.h>
 
-int main(){
+int main(int argc, char *argv[]){
 	std::cout<<"Hello Worlds!"<<std::endl;
+
+	int rank, size;
+	MPI_Init(&argc, &argv); //Start up MPI
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank); //Get current processor number
+	MPI_Comm_size(MPI_COMM_WORLD, &size); //Get number of processors in communicator
+	std::cout<<"Process "<<rank<<" of "<<size<<" online"<<std::endl;
+
+	MPI_Finalize();
+
+	return 0;
 }
