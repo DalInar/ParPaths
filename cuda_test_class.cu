9652dbe58193e9b86670111c0ccdeb8e921ebbb2
DalInar <oryx@umich.edu>
2014-12-11 18:49:00 -0500

    debug

--- cuda_test_class.cu
+++ cuda_test_class.cu
@@ -18,6 +18,7 @@ cuda_test_class::cuda_test_class(int N_){
 
 bool cuda_test_class::check(){
 	for(int i=0; i<N; i++){
+		std::cout<<i<<std::endl;
 		if(a[i] + b[i] != c[i]){
 			std::cout<<"Error! Sum not correct for index "<<i<<std::endl;
 			std::cout<<a[i]<<" + "<<b[i]<<" != "<<c[i]<<std::endl;
