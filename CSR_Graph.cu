5816c85e0249571388b54b18ba609085f213b4e7
DalInar <oryx@umich.edu>
2014-12-12 16:47:51 -0500

    Started adding test gpu code to CSR_Graph

--- CSR_Graph.cu
+++ CSR_Graph.cu
@@ -7,8 +7,36 @@
 
 #include "CSR_Graph.h"
 
+__global__ void test_add(int *a, int *b, int *c){
+	c[blockIdx.x] = a[blockIdx.x] + b[blockIdx.x];
+}
+
 void CSR_Graph::BellmanFordGPU(int source_, std::vector <int> &predecessors, std::vector <double> &path_weight){
 
+	int N=5;
+	int *a, *b, *c;
+	int *d_a, *d_b, *d_c;
+
+	int size=N*sizeof(int);
+	a = (int *) malloc(size);
+	b = (int *) malloc(size);
+	c = (int *) malloc(size);
+	cudaMalloc((void **) & d_a, size);
+	cudaMalloc((void **) & d_b, size);
+	cudaMalloc((void **) & d_c, size);
+
+	//Copy inputs to device
+	cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+
+	test_add<<<N,1>>>(d_a,d_b,d_c);
+
+	//Copy results back to host
+	cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+
+	//cleanup
+	cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+	free(a); free(b); free(c);
 }
 
 
