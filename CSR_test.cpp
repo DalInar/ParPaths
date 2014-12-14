9d62a64ee2239a2a999aee2fe76523d093db7cb6
DalInar <oryx@umich.edu>
2014-12-14 17:13:05 -0500

    de

--- CSR_test.cpp
+++ CSR_test.cpp
@@ -44,6 +44,13 @@ int main(){
 	G_gpu.print_graph_to_file(filename);
 	//std::cout<<"Bellman-Ford time: "<<G_gpu.BellmanFord(source, predecessors_BF, path_weight_BF)<<std::endl;
 	std::cout<<"Dijkstra time: "<<G_gpu.Dijkstra(source, predecessors, path_weight)<<std::endl;
+
+	for(int i=0; i < 100; i++){
+			std::cout<<"V: "<<i<<std::endl;
+			std::cout<<"Pred: "<< predecessors[i]<<"\t"<<predecessors_BF_gpu[i]<<std::endl;
+			std::cout<<"PW: "<<path_weight[i]<<"\t"<<path_weight_BF_gpu[i]<<std::endl;
+		}
+
 	std::cout<<"Bellman-Ford GPU time: "<<G_gpu.BellmanFordGPU_Split(source, predecessors_BF_gpu, path_weight_BF_gpu)<<std::endl;
 	std::cout<<"Dijkstra SSSP Valid: "<<G_gpu.validate(source, predecessors, path_weight)<<std::endl;
 
