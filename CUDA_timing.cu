0a6407b02356e0a4346f22ccbbd7f2baea46d524
DalInar <oryx@umich.edu>
2014-12-17 12:10:11 -0500

    More timing from CUDA_timing.cu

--- CUDA_timing.cu
+++ CUDA_timing.cu
@@ -9,8 +9,8 @@ __global__ void empty_kernel(){
 
 int main(){
 
-	int * data;
-	int * d_data;
+	double * data;
+	double * d_data;
 
 	std::ofstream output;
 	output.open("CUDA_kernel_timing.txt");
@@ -18,7 +18,7 @@ int main(){
 	int num_blocks;
 	int num_threads;
 
-	int trials = 10000;
+	int trials = 100000;
 
 	boost::timer::cpu_timer timer;
 	for(int i=0; i<trials; i++){
@@ -27,7 +27,7 @@ int main(){
 	}
 	timer.stop();
 
-	output<<"10000 kernel launches, <<<1,1024>>>, average time = "<<( (double) timer.elapsed().wall / 1000000000.0 ) / trials<< std::endl;
+	output<<trials<<" kernel launches, <<<1,1024>>>, average time = "<<( (double) timer.elapsed().wall / 1000000000.0 ) / trials<< std::endl;
 
 	timer.start();
 	for(int i=0; i<trials; i++){
@@ -36,7 +36,7 @@ int main(){
 	}
 	timer.stop();
 
-	output<<"10000 kernel launches, <<<1024,1>>>, average time = "<< ( (double) timer.elapsed().wall / 1000000000.0 ) / trials<<std::endl;
+	output<<trials<<" kernel launches, <<<1024,1>>>, average time = "<< ( (double) timer.elapsed().wall / 1000000000.0 ) / trials<<std::endl;
 
 	timer.start();
 	for(int i=0; i<trials; i++){
@@ -45,8 +45,34 @@ int main(){
 	}
 	timer.stop();
 
-	output<<"10000 kernel launches, <<<64,64>>>, average time = "<< ( (double) timer.elapsed().wall / 1000000000.0 ) / trials<<std::endl;
+	output<<trials<<" kernel launches, <<<64,64>>>, average time = "<< ( (double) timer.elapsed().wall / 1000000000.0 ) / trials<<std::endl;
+
+	std::vector <int> num_doubles;
+	num_doubles.push_back(100);
+	num_doubles.push_back(1000);
+	num_doubles.push_back(10000);
+	num_doubles.push_back(100000);
+	num_doubles.push_back(1000000);
+	num_doubles.push_back(10000000);
+
+	trials = 100;
+	int data_size;
+	for(int i=0; i<num_doubles.size(); i++){
+		data_size = num_doubles[i] * sizeof(double);
+		data = (double *) malloc(data_size);
+		cudaMalloc((void **) & d_data, data_size);
+
+		timer.start();
+		for(int j=0; j< trials; j++){
+			cudaMemcpy(d_data, data, data_size, cudaMemcpyHostToDevice);
+			cudaMemcpy(data, d_data, data_size, cudaMemcpyDeviceToHost);
+		}
+		timer.stop();
+
+		output<<trials<<" memory writes and reads to GPU of size "<<data_size<<", average time = "<< ( (double) timer.elapsed().wall / 1000000000.0 ) / trials<<std::endl;
+
+	}
 
 
-
+	return 0;
 }
